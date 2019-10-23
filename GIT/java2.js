d082a0d9e1e0928b9e89962a98f071cabe7c71fe
Jakub Strączek <straczekjakub@endor.local>
2019-10-23 11:46:58 +0200

    21.10 23.10.2019

---
+++ GIT/java2.js
@@ -0,0 +1,44 @@
+"var lub let"
+
+var //globalne zmienne
+let //tymczasowe zmienne
+
+var zmiennaPierwsza; //camel notation
+let zmiennaPierwsza; //pascal notation
+let zmienna_pierwsza; //under scroll notation
+
+var str = 'aaa',
+    str2 = 'bbb';
+
+var str = 'aaa';
+var str2 = 'bbb';
+
+var str3, str4, str5, str6;
+
+if(true) {
+    
+}
+
+document.write('<b>' + (a * 0.22) + '</b>');
+document.write('<b>' + (a * 0.22) + '</b>');
+document.write('<b>' + (a * 0.22) + '</b>');
+
+
+
+var func1 = function() {
+    return 1;
+}
+var boll = true;
+var l = 50;
+var i;
+var j;
+var len;
+var hei;
+
+if(boll == true){
+
+};
+
+if(typeof boll == Boolean) {
+
+};
