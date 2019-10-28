5d3f9d1df198d0a13c9688af2b0d3663c926513d
Jakub Strączek <straczekjakub@endor.local>
2019-10-28 11:27:05 +0100

    28.10 28.10.2019

---
+++ GIT/java3.js
@@ -0,0 +1,18 @@
+var text = "Ala ma kota";
+for(let i = 0; i < text.length; i++)
+    document.writeln(text[i]);          
+
+var num = 16;
+for(let i = num; i > 0; i--)
+    console.log(i.toString(16));
+
+ var hex ="ce";
+ alert(parseInt(hex, 16));
+
+ var float = 109.67;
+ console.log(parseInt(float));
+
+  var f_text = "399.5677";
+ console.log(parseFloat(f_text));
+ console.log(parseFloat(f_text) + float);
+ console.log(f_text + float);
