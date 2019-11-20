782cf8a2625adb0197c62b8dcec6ff2af683e8f9
Jakub Strączek <straczekjakub@endor.local>
2019-11-20 12:24:05 +0100

    tablice dalej 20.11.2019

---
+++ GIT/java5.js
@@ -0,0 +1,9 @@
+var zzz = "Ala \"ma\" kota\nSierotka ma rysia";    //gdy chcemy dać drugi cudzysłów w środku zdania trzeba dać jeden przed jego początek i przed jego zamknięciem
+var yyy = 'Ala \'ma\' kota';                       // "/n" nowa linia w konsoli
+console.log(yyy);
+
+\b
+\t
+\v
+zzz.length;
+zzz[3];
